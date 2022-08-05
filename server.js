25da1ff995d1e5308c005d5dd2e9949652e9ce13
Vu Hung <phamvuhung2710@gmail.com>
2022-08-05 13:09:46 +0700

    update

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.post('/download/', (req, res) => {
   nameMusic = nameMusic.replaceAll(' ', '-').trim()
 
   var YD = new YoutubeMp3Downloader({
-    ffmpegPath: './ffmpeg/bin/ffmpeg.exe', // FFmpeg binary location
-    outputPath: './public', // Output file location (default: the home directory)
+    ffmpegPath: 'ffmpeg/bin/ffmpeg.exe', // FFmpeg binary location
+    outputPath: 'public', // Output file location (default: the home directory)
     youtubeVideoQuality: 'highestaudio', // Desired video quality (default: highestaudio)
     queueParallelism: 2, // Download parallelism (default: 1)
     progressTimeout: 2000, // Interval in ms for the progress reports (default: 1000)
