83c3a1903ff714f43576b6827596a78cd9d918e1
Vu Hung <phamvuhung2710@gmail.com>
2022-08-05 12:42:32 +0700

    update

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ app.post('/download/', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
   console.log(`Server started running`)
 })
 
